77441040726bacaf3eaf8f98a6cd9aa8be114ad7
McBikan <brianjetfenix@gmail.com>
2024-04-23 21:20:09 -0500

    Clase Piece completado

--- Sprint2/src/main/java/org/example/sprinttwo/englishdraughts/Piece.java
+++ Sprint2/src/main/java/org/example/sprinttwo/englishdraughts/Piece.java
@@ -1,4 +1,27 @@
 package org.example.sprinttwo.englishdraughts;
 
 public class Piece {
+    private int color;
+    private boolean isKing;
+
+    public Piece(int color) {
+        this.color = color;
+        this.isKing = false;
+    }
+
+    public void makeKing() {
+        this.isKing = true;
+    }
+
+    public boolean isKing() {
+        return this.isKing;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
 }
